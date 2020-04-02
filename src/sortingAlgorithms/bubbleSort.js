874deee58fb3d8b4fd03b01c3018b68629155b9e
nem <nem.alt001@gmail.com>
2020-04-02 14:33:15 -0600

    Finished implementation of bubble sort

--- src/sortingAlgorithms/bubbleSort.js
+++ src/sortingAlgorithms/bubbleSort.js
@@ -1,10 +1,20 @@
 export default function bubbleSort(arr) {
     const anims = [];
     if(arr.length <= 1) return arr;
-    bubbleSortHelper(arr, anims);
-    return anims;
-}
 
-function bubbleSortHelper() {
-    
+    for(let i = 0; i < arr.length - 1; i++) {
+        for(let j = 0; j < arr.length - i - 1; j++) {
+            const anim = {};
+            anim.comparison = [i, j];
+            anim.swap = null;
+            if(arr[j] > arr[j + 1]) {
+                const holder = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = holder;
+                anim.swap = [j, j + 1];
+                anims.push(anim);
+            }
+        }
+    }
+    return anims;
 }
