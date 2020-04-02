bd3eaa70e21b4d003ae9e6cb1af582a9fc0c0829
nem <nem.alt001@gmail.com>
2020-04-02 09:17:08 -0600

    Added heap sort template

---
+++ src/sortingAlgorithms/heapSort.js
@@ -0,0 +1,12 @@
+export default function heapSort(arr) {
+    const anims = [];
+    if(arr.length <= 1) return arr;
+    const auxArray = arr;
+    heapSortHelper(auxArray);
+    return anims;
+}
+
+function heapSortHelper(arr, low, high, anims) {
+}
+
+
