9072b1e5323be2a6a26a3b93a507bfe293646084
nem <nem.alt001@gmail.com>
2020-04-03 09:20:03 -0600

    Updates

---
+++ precache-manifest.bb2637b07f096012639fa249986f282b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1248c3ff73253ac9cc97106f1040b79a",
+    "url": "/menmai/VisSort/index.html"
+  },
+  {
+    "revision": "5e4907c1812b4a0fab43",
+    "url": "/menmai/VisSort/static/css/main.d6cd98f1.chunk.css"
+  },
+  {
+    "revision": "d2fed6a08581938f351d",
+    "url": "/menmai/VisSort/static/js/2.464a9f2e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/menmai/VisSort/static/js/2.464a9f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4907c1812b4a0fab43",
+    "url": "/menmai/VisSort/static/js/main.e3f0b116.chunk.js"
+  },
+  {
+    "revision": "eae3f9ddddc2a32a5712",
+    "url": "/menmai/VisSort/static/js/runtime-main.d4e6d51a.js"
+  }
+]);
